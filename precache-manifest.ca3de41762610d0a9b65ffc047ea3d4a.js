61ffd61fe6c1eba29962cf9d68e0ed5e169ece26
Bart Gravesteijn <bart@MacBook-Air-van-Bart.local>
2024-07-01 10:10:13 +0200

    Updates

---
+++ precache-manifest.ca3de41762610d0a9b65ffc047ea3d4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1837a4237248309a5623167cc96ba498",
+    "url": "/artist-events/index.html"
+  },
+  {
+    "revision": "7c058030e97df8683e17",
+    "url": "/artist-events/static/css/2.876f1645.chunk.css"
+  },
+  {
+    "revision": "1c0de03027e121f14b88",
+    "url": "/artist-events/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7c058030e97df8683e17",
+    "url": "/artist-events/static/js/2.e424ce8f.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/artist-events/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "1c0de03027e121f14b88",
+    "url": "/artist-events/static/js/main.a0c495a6.chunk.js"
+  },
+  {
+    "revision": "97262d6d0b08bfe96428",
+    "url": "/artist-events/static/js/runtime~main.6a301817.js"
+  }
+]);
